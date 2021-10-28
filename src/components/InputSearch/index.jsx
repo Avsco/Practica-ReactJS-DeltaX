bafd0a941e1e0d78ff449949dfccf7a6e9b0bb49
Alan Benavides <bruno@coastt.org>
2021-10-28 12:47:18 -0400

    add: create component inputSearch

---
+++ src/components/InputSearch/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function InputSearch({ searchBook }) {
+	const [contentSearch, setContentSeach] = useState("");
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		searchBook(contentSearch);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleOnSubmit}>
+				<label>Search: </label>
+				<input
+					autoFocus
+					type="text"
+					value={contentSearch}
+					onChange={(e) => setContentSeach(e.target.value)}
+				/>
+				<button type="submit">search</button>
+			</form>
+		</div>
+	);
+}
+
+export default InputSearch;
