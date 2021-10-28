23443d998557a21754709faa93b59947c7d6fc87
Alan Benavides <bruno@coastt.org>
2021-10-28 12:47:36 -0400

    add: implement view Search

--- src/views/Search/index.jsx
+++ src/views/Search/index.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import InputSearch from "../../components/InputSearch";
+import { searchBook } from "../../services/books";
+import ListHomeBooks from "../../components/ListHomeBooks";
+import { useBooks } from "../../hooks/useBooks";
+
 function Search() {
-	return <div>SEARCH</div>;
+	const { books, setBooks, changeShelfBook } = useBooks();
+	const [error, setError] = useState(null);
+
+	const searchBookByInput = async (data) => {
+		const response = await searchBook(data);
+		if (response.error) {
+			setError(response.error);
+			setBooks(() => []);
+			return;
+		}
+		setBooks(() => response);
+	};
+
+	return (
+		<div>
+			<h1>SEARCH</h1>
+			<InputSearch searchBook={searchBookByInput} />
+			{error ? <p>No hay resultados</p> : <ListHomeBooks books={books} changeShelfBook={changeShelfBook} />}
+		</div>
+	);
 }
 
 export default Search;
