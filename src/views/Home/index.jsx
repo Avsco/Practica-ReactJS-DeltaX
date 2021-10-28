f8ecd33e0897aa78cf3cc604ebbf70b98c9c5c9f
Alan Benavides <bruno@coastt.org>
2021-10-28 12:08:27 -0400

    add: implement home

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+import { getAllBooks, updateBook } from "../../services/books";
+import ListHomeBooks from "../../components/ListHomeBooks";
+
 function Home() {
-	return <div>HOME</div>;
+	const [books, setBooks] = useState([]);
+
+	const changeShelfBook = ({ value, title }) => {
+		setBooks((lb) =>
+			lb.map((b) => {
+				if (title == b.title) {
+					b.shelf = value;
+					updateBook(b.id, value);
+				}
+				return b;
+			})
+		);
+	};
+
+	useEffect(async () => {
+		const response = await getAllBooks();
+		setBooks(() => response);
+	}, []);
+
+	const filterdBooks = [
+		{
+			nameSection: "Currently Reading",
+			books: books.filter((b) => b.shelf == "currentlyReading"),
+		},
+		{
+			nameSection: "Want to Read",
+			books: books.filter((b) => b.shelf == "wantToRead"),
+		},
+		{
+			nameSection: "Read",
+			books: books.filter((b) => b.shelf == "read"),
+		},
+	];
+
+	return (
+		<div>
+			{books &&
+				filterdBooks.map((b) => (
+					<section key={b.nameSection}>
+						<h2>{b.nameSection}</h2>
+						<ListHomeBooks books={b.books} changeShelfBook={changeShelfBook} />
+					</section>
+				))}
+		</div>
+	);
 }
 
 export default Home;
